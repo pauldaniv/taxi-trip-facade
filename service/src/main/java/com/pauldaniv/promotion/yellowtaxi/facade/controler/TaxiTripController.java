7a94e3347c0bf778f45c3f77f8936b875c02e6d4
Pavlo <paulsegfault@gmail.com>
2023-05-09 12:05:39 +0300

    Enable kafka topic creation

---
+++ service/src/main/java/com/pauldaniv/promotion/yellowtaxi/facade/controler/TaxiTripController.java
@@ -0,0 +1,9 @@
+package com.pauldaniv.promotion.yellowtaxi.facade.controler;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/v1")
+public class TaxiTripController         {
+}
