4d1c2b86aa00879cb5cc2d72a4aa8199e2072b47
Pavlo <paulsegfault@gmail.com>
2023-05-12 16:10:44 +0300

    Update redis configs

--- service/src/main/java/com/pauldaniv/promotion/yellowtaxi/facade/config/RedisConfig.java
+++ service/src/main/java/com/pauldaniv/promotion/yellowtaxi/facade/config/RedisConfig.java
@@ -1,6 +1,7 @@
 package com.pauldaniv.promotion.yellowtaxi.facade.config;
 
 import org.springframework.boot.autoconfigure.cache.RedisCacheManagerBuilderCustomizer;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
@@ -10,6 +11,7 @@ import org.springframework.data.redis.serializer.RedisSerializationContext;
 import java.time.Duration;
 
 @Configuration
+@EnableCaching
 public class RedisConfig {
     @Bean
     public RedisCacheConfiguration cacheConfiguration() {
@@ -23,8 +25,6 @@ public class RedisConfig {
     @Bean
     public RedisCacheManagerBuilderCustomizer redisCacheManagerBuilderCustomizer() {
         return (builder) -> builder
-                .withCacheConfiguration("itemCache",
-                        RedisCacheConfiguration.defaultCacheConfig().entryTtl(Duration.ofSeconds(3)))
                 .withCacheConfiguration("customerCache",
                         RedisCacheConfiguration.defaultCacheConfig().entryTtl(Duration.ofMinutes(5)));
     }
