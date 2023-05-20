e6e8c9c47f1f879538832805642c76ae8112a8e4
Pavlo <pavlodaniv@Sombra-L411.local>
2023-05-20 17:46:58 +0300

    Fix tests being failing due to missing db properties

--- service/src/test/java/com/pauldaniv/promotion/yellowtaxi/facade/service/TaxiTripFacadeApplicationTests.java
+++ service/src/test/java/com/pauldaniv/promotion/yellowtaxi/facade/service/TaxiTripFacadeApplicationTests.java
@@ -1,9 +1,15 @@
 package com.pauldaniv.promotion.yellowtaxi.facade.service;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+// for some reason when test is loaded, it's trying to find database
+// which is not used for this service, so we disable it as following
+@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class})
 class TaxiTripFacadeApplicationTests {
 
 	@Test
