096a62190618a7dbfafcf605f4f7bb765c877f06
Pavlo <paulsegfault@gmail.com>
2023-09-05 16:15:45 +0300

    Post cleanup

--- service/src/test/java/com/pauldaniv/promotion/yellowtaxi/facade/test/service/StatsServiceTest.java
+++ service/src/test/java/com/pauldaniv/promotion/yellowtaxi/facade/test/service/StatsServiceTest.java
@@ -9,11 +9,7 @@ import org.testng.annotations.Test;
 import redis.clients.jedis.JedisPooled;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.eq;
-import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.reset;
-import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
